92c703a25acd2065bf1be1185624c3452fffb6cf
Younji Woo <younjiwoo@Younjis-MacBookPro.local>
2022-01-04 17:00:33 +0900

    added cart, meal and meal item components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/AvailableMeals';
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<Fragment>
+			<Header />
+			<main>
+				<Meals />
+			</main>
+		</Fragment>
+	);
 }
 
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,6 @@
 import { Fragment } from 'react';
-
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals.jpeg';
 import classes from './Header.module.css';
 
 const Header = (props) => {
