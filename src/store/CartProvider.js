3a72f60d5c2b8e2bcd9be243e8a822f6e454fc69
Younji Woo <younjiwoo@Younjis-MacBookPro.local>
2022-01-06 16:35:14 +0900

    finished updating cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,65 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-	const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
 
-	const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+	if (action.type === 'ADD') {
+		// concat은 new array를 return 함.
+		// (참고: push는 원래 array에 추가함)
+
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+
+		const existingCartItem = state.items[existingCartItemIndex];
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item);
+		}
+
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action.type === 'REMOVE') {
+	}
+	return defaultCartState;
+};
+
+const CartProvider = (props) => {
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
+
+	const addItemToCartHandler = (item) => {
+		dispatchCartAction({ type: 'ADD', item });
+	};
+
+	const removeItemFromCartHandler = (id) => {
+		dispatchCartAction({ type: 'REMOVE', id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
